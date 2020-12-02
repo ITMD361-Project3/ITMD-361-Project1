cf337d24c3abb62b26b697005cd7109c1af4f2ad
Meg <70302885+meg-mir@users.noreply.github.com>
2020-12-01 20:22:14 -0600

    edit js

--- js/map.js
+++ js/map.js
@@ -1,24 +1,42 @@
 function initMap () {
-var map = new google.maps.Map (document.getElementById('map'), {
-  center: {lat: 18.2550, lng: -66.4670},
-  zoom: 5
-}};
+  var defaultCenter = new google.maps.LatLng(18.2550,-66.4670);
+  var mapOptions = {
+    center: defaultCenter,
+    zoom: 10
+  };
+  
+  var myLocation =  new google.maps.LatLng(18.2550,-66.4670);
 
-var myMap = new google.maps.Map (el, mapOptions);
-var marker = new google.maps.Marker ({
-  position: myLocation,
-  map: myMap,
-  animation: google.maps.Animation.DROP,
-  icon: 'pricon.jpg'
-});
+  var mapElement = document.getElementById('map');
 
-var infoGPS = new google.maps.InfoWindow({
-    content: contentGPS,
-});
-
-var contentStrig = '<h2 id="maptitle">Scroll through the beautiful island I am from:</h2>;
-var infowindow = new google.maps.InfoWindow({
-  content: contentString,
-  maxWidth: 200,
-});
+  var map = new google.maps.Map (mapElement, mapOptions);
+  
+  var marker = new google.maps.Marker ({
+    position: myLocation,
+    map: map,
+    animation: google.maps.Animation.DROP,
+    icon: 'media/pricon.jpg'
+  }); 
+  
+  var contentString = 
+    '<h2 id="maptitle">Scroll through the beautiful island I am from:</h2>';
+  
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    maxWidth: 200,
+  });
+   
+  marker.addListener('mouseover', function() {
+    infowindow.open(map, this);
+  });
+  
+  marker.addListener('mouseout', function() {
+    infowindow.close();
+  });
+  
+  marker.addListener('click', function() {
+    infowindow.open(map, this);
+  });
+ 
+ google.maps.event.addDomListener(window, 'load', initMap);
 }
